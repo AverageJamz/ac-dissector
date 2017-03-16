4bcecae3ce10a7f4f70666ec932f64dbffa2dbc8
Jamz <jamz@MNNOIS005.archant.co.uk>
2017-03-16 13:41:39 +0000

    Finished evtMessageBox (0x00000004)

--- GameEvents/evtMessageBox.lua
+++ GameEvents/evtMessageBox.lua
@@ -1,5 +1,12 @@
 -- 0x00000004
 
+fields.EvtMessageBoxText        = ProtoField.string("ac.evt.msg_box.text", "Text")
+
 function evtMessageBox(event, tree)
   tree:append_text(" (Message box)")
+
+  local text_len = event(4, 2):le_uint()
+  local text_pad = calc_pad(text_len + 2, 4)
+
+  tree:add(fields.EvtMessageBoxText, event(6, text_len + text_pad))
 end
